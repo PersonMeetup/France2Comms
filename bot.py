27c367f15f410ab5345d24e6cc9130d1f7cbc037
Leslie Swan <42787368+PersonMeetup@users.noreply.github.com>
2021-02-11 20:39:49 -0700

    add photos uploading

--- bot.py
+++ bot.py
@@ -3,10 +3,11 @@ import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from configparser import ConfigParser
-import os
+import os; import logging
 from twitter import relay
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv()      #V{filename='status.log',}
+logging.basicConfig(format='[%(asctime)s:%(levelname)s] %(message)s', level=logging.INFO)
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -49,7 +50,7 @@ def qualified(f):
 
 @bot.event
 async def on_ready():
-    print('Internal Report Check: Logged in as {0.user}'.format(bot))
+    logging.info('Internal Report Check: Logged in as {0.user}'.format(bot))
 
 @bot.event
 async def on_message(message):
@@ -57,28 +58,28 @@ async def on_message(message):
     # Check two things: 1) "Should I be checking messages right now?"
     #                   2) "Is this in the right channel?"
     if (config.getboolean('Server','toggle') == False) or (message.channel.id != config.getint('Server','channel')):
-        #print('Message was posted in the wrong channel, or Twitter functionality is off')
+        logging.debug('Message was posted in the wrong channel, or Twitter functionality is off')
         return
 
     # Now that the message is confirmed to be in the right channel,
     # let's check to see if the user is opted in.
     if str(message.author.id) in config:
         opt = config.getboolean(str(message.author.id),'opt')
-        #print('User settings detected')
+        logging.debug(f'User settings detected: {message.author.id}')
     else:
         opt = config.getboolean('User-Default','opt')
-        #print('No user settings detected, using default')
+        logging.debug(f'No user settings detected ({message.author.id}), using default')
 
     if opt == False:
         return
     else:
-        print('Message passed, attempting to send to Twitter')
-        for attempt in range(5):
-            responcecode = relay.distweet(message)
-            if responcecode == 200:
-                print('Tweet Successful!')
-                break
-            print('Tweet Failure! Reattempting...')
+        logging.info(f'NEW MESSAGE: [{message.content}]')
+        responcecode = relay.distweet(message)
+        try:
+            responcecode == 200
+            logging.info('Tweet Successful!')
+        except:
+            logging.warning("The Tweet didn't go through, AND I HAVE NO IDEA HOW TO FIX THIS PLACE")
 
 
 
@@ -97,9 +98,11 @@ async def _mark(ctx: SlashContext, channel = None):
     if channel:
         configRequest('Server','channel',str(channel.id))
         await ctx.send(content=f"Set {channel.mention} to have it's messages sent to Twitter.",complete_hidden=True)
+        logging.warning(f'Twitter channel ID has been set to {channel.id}!')
     else:
         configRequest('Server','channel',str(ctx.channel.id))
         await ctx.send(content="Set the current channel to have it's messages sent to Twitter.",complete_hidden=True)
+        logging.warning(f'Twitter channel ID has been set to {ctx.channel.id}!')
 
 @slash.slash(
     name='toggle',
@@ -114,6 +117,7 @@ async def _mark(ctx: SlashContext, channel = None):
 @qualified
 async def _toggle(ctx: SlashContext, value: bool):
     configRequest('Server','toggle',str(value))
+    logging.warning(f'Message forwarding has been set to {value}!')
     await ctx.send(content=f'Message forwarding set to `{value}`!',complete_hidden=True)
 
 @slash.slash(name='optin',description="Opt into the bot's Twitter functionality",guild_ids=guild_ids)
--- twitter.py
+++ twitter.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import discord
+import requests
 from TwitterAPI import TwitterAPI
 import os; import re
+import logging
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,11 +15,24 @@ class relay:
     def distweet(message):
         """Decompiles a `discord.Message` method and posts its content to Twitter"""
         status = message.content
-        #if message.attachments:
-        #    file = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', message.attachments[0])
+        if message.attachments:
+            logging.info(f'Message contains attachment, extracting URL... [{(message.attachments[0]).url}]')
+            r = requests.get((message.attachments[0]).url)
+            with open('image.png','wb') as f:
+                f.write(r.content)
 
-        r = api.request('statuses/update', {'status':status[:280]})
-        return r.status_code
+            file = open('./image.png', 'rb')
+            data = file.read()
+            r = api.request('media/upload', None, {'media': data})
+            #print('UPLOAD MEDIA SUCCESS' if r.status_code == 200 else 'UPLOAD MEDIA FAILURE: ' + r.text)
+
+            if r.status_code == 200:
+                media_id = r.json()['media_id']
+                tweet = api.request('statuses/update', {'status':status[:280], 'media_ids':media_id})
+                return tweet.status_code
+
+        tweet = api.request('statuses/update', {'status':status[:280]})
+        return tweet.status_code
 
     def apitest():
         r = api.request('statuses/home_timeline', {'count':5})
